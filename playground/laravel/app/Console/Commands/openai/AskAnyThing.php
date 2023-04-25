ff37e606970f6b14b8cb09fba8c9cf7e9b3905b5
tinghaolai <analth355@gmail.com>
2023-04-25 16:45:29 +0800

    Command modify

--- playground/laravel/app/Console/Commands/openai/AskAnyThing.php
+++ playground/laravel/app/Console/Commands/openai/AskAnyThing.php
@@ -7,14 +7,14 @@ use Illuminate\Support\Facades\Http;
 
 class AskAnyThing extends Command
 {
-    protected $signature = 'command:askGPT';
+    protected $signature = 'ask';
 
     public function handle()
     {
         $apiKey = env('OPENAI_API_KEY');
         $model = 'gpt-3.5-turbo';
         $url = 'https://api.openai.com/v1/chat/completions';
-        $askContent = file_get_contents('./ask-gpt');
+        $askContent = file_get_contents('./ask-gpt.md');
 
         $data = [
             'model' => $model,
@@ -39,7 +39,7 @@ class AskAnyThing extends Command
             $body = $response->body();
             $askResult = json_decode($body, true)['choices'][0]['message']['content'];
 
-            $filePath = './ask-gpt-result';
+            $filePath = './ask-gpt-result.md';
             file_put_contents($filePath, '');
             file_put_contents($filePath, $askResult);
         } else {
