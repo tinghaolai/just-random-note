e2a75f80373e7f247c268a72e7c48a2e9639b708
tinghaolai <analth355@gmail.com>
2023-06-19 00:40:57 +0800

    Add test for zero configuration

--- playground/laravel_10_explore/routes/web.php
+++ playground/laravel_10_explore/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Services\TestZeroConfiguration;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test1', function (TestZeroConfiguration $service) {
+    die(get_class($service));
 });
